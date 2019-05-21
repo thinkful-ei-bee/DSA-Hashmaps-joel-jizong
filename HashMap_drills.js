80a5945d7be4920dcfa10b1db9df57deaf62f280
Joel Corey <joelcorey@fastmail.com>
2019-05-21 04:15:23 -0700

    solutions

--- HashMap_drills.js
+++ HashMap_drills.js
@@ -57,7 +57,24 @@ function main() {
     capacity is 8. 8 is multiplied by SIZE_RATIO of 3, increasing capacity to 24.
   */
 
-  
+  const WhatDoesThisDo = function(){
+    let str1 = 'Hello World.';
+    let str2 = 'Hello World.';
+    let map1 = new HashMap();
+    map1.set(str1,10);
+    map1.set(str2,20);
+    let map2 = new HashMap();
+    let str3 = str1;
+    let str4 = str2;
+    map2.set(str3,20);
+    map2.set(str4,10);
+    
+    console.log(map1.get(str1));
+    console.log(map2.get(str3));
+  }
+  WhatDoesThisDo();
+
+  console.log('test: ' + 90 % 4);
 }
 
 main();
